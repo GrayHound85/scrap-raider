6d90c94280dd88c06b6500b7e3030aa70a17b873
GrayHound85 <143196243+GrayHound85@users.noreply.github.com>
2025-10-02 20:17:58 +0100

    Tweek generation values and added room generation on points

--- NPA_game/dungeon/scripts/dungeon.gd
+++ NPA_game/dungeon/scripts/dungeon.gd
@@ -5,10 +5,23 @@ extends Node2D
 
 # Classes
 var poisson = PoissonDiscSampling.new()
+var room_generator = RoomGenerator.new()
 
 # enums & consts
-enum room {WIDTH = 4, HEIGHT = 3,}
-enum gen {PADDING = 1, WIDTH = 30, HEIGHT = 30, K = 30, TILESIZE = 32}
+enum room {
+	MIN_WIDTH = 3,
+	MAX_WIDTH = 6,
+	MIN_HEIGHT = 3,
+	MAX_HEIGHT = 7,
+}
+enum gen {
+	PADDING = 1,
+	WIDTH = 60,
+	HEIGHT = 60,
+	K = 30,
+	TILESIZE = 32,
+	DENSITY = 6 # Lower value = higher density. Value = distance between rooms
+}
 
 
 func _ready() -> void:
@@ -21,11 +34,17 @@ func _ready() -> void:
 		return
 	
 	var region_size = Vector2(gen.TILESIZE * gen.WIDTH, gen.TILESIZE * gen.HEIGHT)
-	var radius = gen.TILESIZE * 1.5 + gen.PADDING
+	var radius = gen.TILESIZE * gen.DENSITY + gen.PADDING
 	var points = poisson.generate_points(radius, region_size, gen.K)
 	
+	var occupied: Dictionary = room_generator.generate_rooms(points, room, gen)
+
+
+	for tile in occupied.keys():
+		tile_map_layer.set_cell(tile, 1, Vector2i(0, 0))
+
+	# Creates tile at position of points
 	print(points)
-	
 	var tile_size = Vector2i(gen.TILESIZE, gen.TILESIZE)
 	for point in points:
 		var tile_coord = Vector2i(
@@ -34,4 +53,3 @@ func _ready() -> void:
 		)
 		if tile_coord.x >= 0 and tile_coord.x < gen.WIDTH and tile_coord.y >= 0 and tile_coord.y < gen.HEIGHT:
 			tile_map_layer.set_cell(tile_coord, 0, Vector2i(0, 0))
-		
---
+++ NPA_game/dungeon/scripts/room_generator.gd
@@ -0,0 +1,45 @@
+extends Node
+
+class_name RoomGenerator
+
+# Variables
+var occupied := {}
+var tile: int = 0
+var max_failed_attempts := 3
+
+func generate_rooms(points: Array, room, gen):
+	var points_copy = points.duplicate()
+	while points_copy.size() > 0 and max_failed_attempts > 0:
+		for i in range(points_copy.size() - 1, -1, -1):
+			var point = points_copy[i]
+			var padding =  gen.PADDING * 2
+			var room_w = randi_range(room.MIN_WIDTH + padding, room.MAX_WIDTH + padding)
+			var room_h = randi_range(room.MIN_HEIGHT + padding, room.MAX_HEIGHT + padding)
+			
+			var tile_coord = Vector2i(
+				int(round(point.x / gen.TILESIZE)),
+				int(round(point.y / gen.TILESIZE))
+			)
+			
+			@warning_ignore("integer_division")
+			var top_left = tile_coord - Vector2i(room_w / 2, room_h / 2)
+			
+			if can_place_room(top_left, room_w, room_h):
+				for x in range(room_w):
+					for y in range(room_h):
+						var coord = top_left + Vector2i(x,y)
+						occupied[coord] = tile
+				points_copy.remove_at(i)
+				max_failed_attempts = 3
+			else:
+				max_failed_attempts -= 1
+	
+	return occupied
+
+
+func can_place_room(top_left: Vector2i, w: int, h: int):
+	for x in range(w):
+		for y in range(h):
+			if (top_left + Vector2i(x,y)) in occupied:
+				return false
+	return true
