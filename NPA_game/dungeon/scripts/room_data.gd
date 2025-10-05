f12f72e8197063330af2c875f88b8820a1b94022
GrayHound85 <143196243+GrayHound85@users.noreply.github.com>
2025-10-05 15:09:36 +0100

    Continued working on new dungeon gen. Added simple player movement

--- NPA_game/dungeon/scripts/room_data.gd
+++ NPA_game/dungeon/scripts/room_data.gd
@@ -1,1 +1,25 @@
 extends Node2D
+
+@export var room_name: String
+
+var door_points: Array[Vector2i]
+var loot_points: Array[Vector2i]
+var enemy_points: Array[Vector2i]
+
+func _ready() -> void:
+	var doors = $Doors
+	var loot = $LootSpawns
+	var enemies = $EnemySpawns
+	
+	doors.visible = false
+	loot.visible = false
+	enemies.visible = false
+	
+	for pos in doors.get_used_cells():
+		door_points.append(pos)
+	
+	for pos in loot.get_used_cells():
+		loot_points.append(pos)
+		
+	for pos in enemies.get_used_cells():
+		enemy_points.append(pos)
---
+++ NPA_game/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var speed = 5000
+
+func get_input(delta):
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed * delta
+
+
+func _physics_process(delta: float) -> void:
+	get_input(delta)
+	move_and_slide()
