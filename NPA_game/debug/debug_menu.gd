637409116b564b89acd027273a6e08d9cf0609d6
GrayHound85 <143196243+GrayHound85@users.noreply.github.com>
2025-10-06 21:19:23 +0100

    Fixed debug menu feature - show coord of tile that is being hovered over

--- NPA_game/debug/debug_menu.gd
+++ NPA_game/debug/debug_menu.gd
@@ -2,16 +2,19 @@ extends CanvasLayer
 
 @onready var tile_location_label: Label = $Label
 @onready var camera = get_viewport().get_camera_2d()
+@onready var tile_map: TileMapLayer = get_tree().get_root().find_child("TileMapLayer", true, false)
+
 
 var view_to_world
 
 func _ready() -> void:
 	pass
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		var world_pos = camera.to_global(get_viewport().get_mouse_position())
-		print(world_pos)
-		#print(get_viewport().get_mouse_position())
-		#print(get_viewport().get_global_mouse_position())
+func _process(delta: float) -> void:
+	var mouse_pos_screen: Vector2 = get_viewport().get_mouse_position()
+	var world_pos = camera.get_canvas_transform().affine_inverse() * mouse_pos_screen
+	var local_pos: Vector2 = tile_map.to_local(world_pos)
+	var tile_coord: Vector2i = tile_map.local_to_map(local_pos)
+		
+	tile_location_label.text = "Tile: %s" % str(tile_coord)
 	
